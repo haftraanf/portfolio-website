6fd2764cb2780524161df1aac87d43e445907094
Ha Tran <trank58@mcmaster.ca>
2023-10-08 17:48:27 -0400

    Added footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,21 @@
-import Image from 'next/image'
+'use client'
 import HeroSection from '@/components/HeroSection'
 import ProjectSection from '@/components/ProjectSection'
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  let start = new Date();
+  let year = start.getFullYear().toString();
+
   return (
-    <main className="min-h-screen bg-[#111111]">
+    <main>
       <div className="container mx-auto text-center px-7 py-20">
         <HeroSection />
         <ProjectSection />
       </div>
+      <footer className="text-xs text-center fixed bottom-0 w-full">
+        © Ha Tran {year} - All Rights Reserved.
+      </footer>
     </main>
   
   )
