be4607733813c64ecc576b316ced5e0eb18a86c7
Ha Tran <trank58@mcmaster.ca>
2023-10-08 17:51:23 -0400

    Fixed splitting background color

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,8 +1,9 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
-const ProjectSection = () => {
 
+
+const ProjectSection = () => {
     const projectsList = [
         {
             id: 1,
@@ -19,29 +20,31 @@ const ProjectSection = () => {
     ]
 
   return (
-    <div className="md:h-screen bg-[#111111]">
+    <div className="md:h-screen">
         <h2 className="text-4xl font-semibold m-16 md:mb-10">
             My Projects
         </h2>
-
-        {projectsList.map(({id, imageSrc, description, gitUrl}) => (
-            <div key={id} className='max-w-screen-md mx-auto shadow-lg shadow-gray-600 rounded-xl m-20 mb-20'>
-                <Image 
-                    src={imageSrc} 
-                    alt={description}  
-                    width={1000}
-                    height={1000}
-                    style={{ display: 'block', margin: '0 auto', borderRadius: '10px'}} 
-                    className="duration-200 hover:scale-105"
-                />
-                <button className='text-2xl px-2 m-4 duration-200 hover:scale-105'
-                        onClick={() => window.open(gitUrl, '_blank')}
-                >
-                    Code
-                </button>
-            </div>
-            ))
-        }    
+        <div>
+            {projectsList.map(({id, imageSrc, description, gitUrl}) => (
+                <div key={id} className='max-w-screen-md mx-auto shadow-lg shadow-gray-600 rounded-xl m-20'>
+                    <Image 
+                        src={imageSrc} 
+                        alt={description}  
+                        width={1000}
+                        height={1000}
+                        style={{ display: 'block', margin: '0 auto', borderRadius: '10px'}} 
+                        className="duration-200 hover:scale-105"
+                    />
+                    <button className='text-2xl px-2 m-4 duration-200 hover:scale-105'
+                            onClick={() => window.open(gitUrl, '_blank')}
+                    >
+                        Code
+                    </button>
+                </div>
+                ))
+            }    
+        </div>
+    
     </div>
   )
 }
