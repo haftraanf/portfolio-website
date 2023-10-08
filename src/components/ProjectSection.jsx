ea22c7422ac5b81ed5367f6207bad6de7cd0452b
Ha Tran <trank58@mcmaster.ca>
2023-10-08 15:26:59 -0400

    Fixed project links

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 const ProjectSection = () => {
@@ -7,11 +8,13 @@ const ProjectSection = () => {
             id: 1,
             imageSrc: "/help.png",
             description: "A discord bot that track one's expenses",
+            gitUrl: "https://github.com/haftraanf/Thriftier",
         },
         {
             id: 2,
             imageSrc: "/4.png",
             description: "Connect Four with GUI added",
+            gitUrl: "https://github.com/haftraanf/ConnectFour",
         },
     ]
 
@@ -21,16 +24,20 @@ const ProjectSection = () => {
             My Projects
         </h2>
 
-        {projectsList.map(({id, imageSrc, description}) => (
+        {projectsList.map(({id, imageSrc, description, gitUrl}) => (
             <div key={id} className='max-w-screen-md mx-auto shadow-lg shadow-gray-600 rounded-xl m-20 mb-20'>
                 <Image 
                     src={imageSrc} 
                     alt={description}  
-                    style={{ width: '100%', height: '80%', display: 'block', margin: '0 auto', borderRadius: '10px'}} 
+                    width={1000}
+                    height={1000}
+                    style={{ display: 'block', margin: '0 auto', borderRadius: '10px'}} 
                     className="duration-200 hover:scale-105"
                 />
-                <button className='text-2xl px-2 m-4 duration-200 hover:scale-105'>
-                Code
+                <button className='text-2xl px-2 m-4 duration-200 hover:scale-105'
+                        onClick={() => window.open(gitUrl, '_blank')}
+                >
+                    Code
                 </button>
             </div>
             ))
